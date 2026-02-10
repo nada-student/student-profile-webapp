ae0e727df640b85536f8582d5e03d8604ac289c9
Nada <nadakhamrunnisa@gmail.com>
2026-02-11 00:23:35 +0530

    Update frontend script

--- frontend/script.js
+++ frontend/script.js
@@ -1,37 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.getElementById("studentForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
 
-  const form = document.getElementById("studentForm");
+  const studentData = {
+    name: document.getElementById("name").value,
+    rollno: document.getElementById("rollno").value,
+    age: document.getElementById("age").value,
+    address: document.getElementById("address").value,
+    contact: document.getElementById("contact").value,
+    email: document.getElementById("email").value
+  };
 
-  form.addEventListener("submit", async function (e) {
-    e.preventDefault();
+  try {
+    const response = await fetch("https://student-backend-s0vi.onrender.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(studentData)
+    });
 
-    const data = {
-      name: document.getElementById("name").value,
-      rollno: document.getElementById("rollno").value,
-      age: document.getElementById("age").value,
-      address: document.getElementById("address").value,
-      contact: document.getElementById("contact").value,
-      email: document.getElementById("email").value
-    };
-
-    try {
-      const response = await fetch("http://127.0.0.1:5000/student", {
-
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      });
-
-      const result = await response.json();
-      alert(result.message);
-      form.reset();
-
-    } catch (error) {
-      alert("Error connecting to backend");
-      console.error(error);
-    }
-  });
-
+    const result = await response.json();
+    alert(result.message);
+  } catch (error) {
+    alert("Error submitting form");
+    console.error(error);
+  }
 });
